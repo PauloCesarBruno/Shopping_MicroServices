673d60d26a708fc09df2d5d0eb1351aae1cc5341
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-02-20 17:42:11 -0300

    Add. Controller, Carga de Dados e refatoração

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// Configurando Swagger v1.
 builder.Services.AddSwaggerGen(c=>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI" });
