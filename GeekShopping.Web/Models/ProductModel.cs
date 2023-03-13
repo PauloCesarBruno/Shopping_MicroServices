0835cde20bea62b885fba393080d3620e250592a
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-03-13 19:46:33 -0300

    Index Pagina Home Sec. 12

--- GeekShopping.Web/Models/ProductModel.cs
+++ GeekShopping.Web/Models/ProductModel.cs
@@ -13,14 +13,14 @@ namespace GeekShopping.Web.Models
 
         public string? SubstrinName()
         {
-            if (Name.Length < 24) return Name;
-            return $"{ Name.Substring(0, 21) } ...";
+            if (Name?.Length < 24) return Name;
+            return $"{ Name?.Substring(0, 21) } ...";
         }
 
         public string? SustringDescription()
         {
-            if (Description.Length < 355) return Description;
-            return $"{ Description.Substring(0, 352) } ...";
+            if (Description?.Length < 355) return Description;
+            return $"{ Description?.Substring(0, 352) } ...";
         }
     }
 }
