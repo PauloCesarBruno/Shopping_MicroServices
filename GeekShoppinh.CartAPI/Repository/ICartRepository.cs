da18edc5cb71aec612f3c536664fac33bac3d6b9
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-03-20 20:54:15 -0300

    Começar aula 151 - Sessão 13

---
+++ GeekShoppinh.CartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using GeekShopping.CartAPI.Data.DataTransferObjects;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task <CartDto> FindCartUserId (string userId);
+        Task <CartDto> SaveOrUpdateCart(CartDto cart);
+        Task <bool> RmoveFromCart(long crtDetailsId);
+        Task <bool> ApplyCoupon(string userId, string couponCode);
+        Task <bool> RemoveCoupon(string userId);
+        Task <bool> ClearCart(string userId);
+    }
+}
