25b7fabd7cc2d9108a1fb6337d4e9c8da51904bd
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-03-30 17:21:10 -0300

    Começar Sessão 17 -  Aula 175

--- GeekShopping.Web/Controllers/HomeController.cs
+++ GeekShopping.Web/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
+
 namespace GeekShopping.Web.Controllers
 {
     public class HomeController : Controller
