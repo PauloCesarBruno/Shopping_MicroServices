ae41c1fe18bc53a4249b21605cbcda9079c8d6f0
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-03-31 02:03:22 -0300

    Começar Sessão 17 -  Aula 175

---
+++ GeekShopping.Web/Models/CartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GeekShopping.CartAPI.Data.DataTransferObjects;
+
+namespace GeekShopping.Web.Models
+{
+    public class CartViewModel
+    {
+        public CartHeaderViewModel CartHeader { get; set; }
+        public IEnumerable<CartDetailViewModel> CartDetails { get; set; }
+    }
+}
