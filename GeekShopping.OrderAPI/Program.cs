9f57843307dd38b14e35d2eeeba0baf03c66c866
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-06-15 19:14:11 -0300

    RabbitMQ Sender e Render Já prontos Continuar da Aula 210 - Sessão 21

---
+++ GeekShopping.OrderAPI/Program.cs
@@ -0,0 +1,102 @@
+using GeekShopping.CartAPI.Repository;
+using GeekShopping.OrderAPI.MessageConsumer;
+using GeekShopping.OrderAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddControllers();
+
+// Configurando Swagger v1.
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.CartAPI", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+       new OpenApiSecurityScheme
+       {
+           Reference = new OpenApiReference
+           {
+               Type = ReferenceType.SecurityScheme,
+               Id = "Bearer"
+           },
+           Scheme = "oauth2",
+           Name = "Bearer",
+           In = ParameterLocation.Header
+       },
+       new List<string>()
+       }
+    });
+});
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MySQLContext>(Options =>
+
+                     Options.UseMySql(connection,
+                     ServerVersion.AutoDetect(connection)));
+
+
+// Configurando e injetando Classes da Pasta Repository:
+var build = new DbContextOptionsBuilder<MySQLContext>();
+build.UseMySql(connection, ServerVersion.AutoDetect(connection));
+
+builder.Services.AddSingleton(new OrderRepository(build.Options));
+
+// Injeção do RabbitMQ
+builder.Services.AddHostedService<RabbitMQCheckoutConsumer>();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
